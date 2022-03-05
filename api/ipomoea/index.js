b8b8a91fe591be10f7df6b81ee90f7eaf57e60a1
gilroymenezes-github <gilroymenezes@gmail.com>
2022-03-05 23:01:32 +0530

    cleanup on commented default code in az fn

--- api/ipomoea/index.js
+++ api/ipomoea/index.js
@@ -4,18 +4,6 @@ const tableService = azure.createTableService()//(connectionString)
 const tableName = "names"
 
 module.exports = async function (context, req) {
-    // context.log('JavaScript HTTP trigger function processed a request.');
-
-    // const name = (req.query.name || (req.body && req.body.name));
-    // const responseMessage = name
-    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    // context.res = {
-    //     // status: 200, /* Defaults to 200 */
-    //     body: responseMessage
-    // };
-
     const getNames = async() => {
         return new Promise((resolve, reject ) => {
             var query = new azure.TableQuery().top(100)
